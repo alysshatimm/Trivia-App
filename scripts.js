335a342fb441aff69811c0479bb4d427ab683008
Alyssha Timm <70657234+alysshatimm@users.noreply.github.com>
2022-02-15 12:22:41 -0500

    questions populate on cards, players can click true or false and get points added to their score

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const game = document.querySelector('#game')
-const score = document.querySelector('#score')
+const scoreDisplay = document.querySelector('#score')
+let score = 0
 
 const genres = [
     {
@@ -58,5 +59,40 @@ function addGenre(genre) {
 genres.forEach(genre => addGenre(genre))
 
 function flipCard() {
-    console.log('clicked')
+    const textDisplay = document.createElement('div')
+    const trueButton = document.createElement('button')
+    const falseButton = document.createElement('button')
+    trueButton.innerHTML = 'True'
+    falseButton.innerHTML = 'False'
+    trueButton.addEventListener('click', getResult)
+    falseButton.addEventListener('click', getResult)
+    textDisplay.innerHTML = this.getAttribute('data-question')
+    this.append(textDisplay, trueButton, falseButton)
+
+
+    const allCards = Array.from(document.querySelectorAll('.card'))
+    allCards.forEach(card => card.removeEventListener('click', flipCard))
+}
+
+function getResult() {
+    const cardOfButton = this.parentElement
+    if (cardOfButton.getAttribute('data-answer') === this.innerHTML) {
+        score = score + parseInt(cardOfButton.getAttribute('data-value'))
+        scoreDisplay.innerHTML = score
+        cardOfButton.classList.add('correct-answer')
+        setTimeout(() => {
+            while (cardOfButton.firstChild) {
+                cardOfButton.removeChild(cardOfButton.lastChild)
+            }
+            cardOfButton.innerHTML = cardOfButton.getAttribute('data-value')
+        }, 300)
+    } else {
+        cardOfButton.classList.add('wrong-answer')
+        setTimeout(() => {
+            while (cardOfButton.firstChild) {
+                cardOfButton.removeChild(cardOfButton.lastChild)
+            }
+            cardOfButton.innerHTML = 0
+        }, 300)
+    }
 }
