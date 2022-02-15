030eefedbc8e29228350c898f8ad39fb321b5339
Alyssha Timm <70657234+alysshatimm@users.noreply.github.com>
2022-02-15 11:42:48 -0500

    cards created in game column and question attributes populating in each card

--- scripts.js
+++ scripts.js
@@ -1,3 +1,38 @@
 const game = document.querySelector('#game')
 const score = document.querySelector('#score')
 
+const film = 11
+const levels = ['easy', 'medium', 'hard']
+function addGenre() {
+    const column = document.createElement('div')
+    column.classList.add('genre-column')
+    column.innerHTML = "this is a genre"
+    game.append(column)
+
+    levels.forEach(level => {
+        const card = document.createElement('div')
+        card.classList.add('card')
+        column.append(card)
+
+        if (level === 'easy') {
+            card.innerHTML = 100
+        } 
+        if (level === 'medium') {
+            card.innerHTML = 200
+        }
+        if (level === 'hard') {
+            card.innerHTML = 300
+        }
+
+        fetch(`https://opentdb.com/api.php?amount=1&category=11&difficulty=${level}&type=boolean`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                card.setAttribute('data-question', data.results[0].question)
+                card.setAttribute('data-answer', data.results[0].correct_answer)
+                card.setAttribute('data-value', card.getInnerHTML())
+            })
+    })
+}
+
+addGenre()
